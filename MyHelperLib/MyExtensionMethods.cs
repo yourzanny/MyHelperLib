21d29c7c62f9f34029bde0bce32852e800d1e8b7
Ali <yourzanny@yahoo.com>
2020-11-09 18:20:00 +0500

    equals method added

---
+++ MyHelperLib/MyExtensionMethods.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyHelperLib
+{
+    public static class MyExtensionMethods
+    {
+        public static bool Equals(this string comparingStr, string comparedStr, bool ignoreCase)
+        => ignoreCase ? comparingStr.ToLower().Equals(comparedStr.ToLower()) : false;
+    }
+}
